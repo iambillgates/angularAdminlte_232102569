8bc551e707cc7a48a26b4687a2baa3c127e2fec5
iambillgates <150753840+iambillgates@users.noreply.github.com>
2025-10-20 14:36:44 +0700

    Update 10/20/2025

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { SidebarComponent } from "../sidebar/sidebar.component";
+import { ContentComponent } from "../content/content.component";
+import { FooterComponent } from "../footer/footer.component";
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-admin',
+  imports: [HeaderComponent, SidebarComponent, ContentComponent, FooterComponent, RouterModule],
+  templateUrl: './admin.component.html',
+  styleUrl: './admin.component.css'
+})
+export class AdminComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: "login", pathMatch: "full" },
+    { path: "admin", component: AdminComponent},
+    { path: "dashboard", component: DashboardComponent },
+    { path: "login", component: LoginComponent},
+    { path: "register", component: RegisterComponent}
+];
