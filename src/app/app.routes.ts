e40cb85e41e238ebd2f68dd13a6f133bee7591f7
iambillgates <150753840+iambillgates@users.noreply.github.com>
2025-11-17 14:44:08 +0700

    Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { AdminComponent } from './admin/admin.component';
 import { RegisterComponent } from './register/register.component';
 import { Dashboard2Component } from './dashboard2/dashboard2.component';
 import { Dashboard3Component } from './dashboard3/dashboard3.component';
+import { MahasiswaComponent } from './mahasiswa/mahasiswa.component';
 
 export const routes: Routes = [
     { path: "", redirectTo: "login", pathMatch: "full" },
@@ -12,6 +13,7 @@ export const routes: Routes = [
     { path: "dashboard", component: DashboardComponent },
     { path: "dashboard2", component: Dashboard2Component},
     { path: "dashboard3", component: Dashboard3Component},
+    { path: "mahasiswa", component: MahasiswaComponent },
     { path: "login", component: LoginComponent},
     { path: "register", component: RegisterComponent}
 ];
